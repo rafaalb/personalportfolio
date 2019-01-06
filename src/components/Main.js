a82fded3b68d5edb8c783fc49c179f7b471a9060
Rafael Pina <rafaa.lb@hotmail.com>
2019-01-06 12:37:57 -0400

    Add success page

--- src/components/Main.js
+++ src/components/Main.js
@@ -32,7 +32,7 @@ class Main extends React.Component {
 
         <article id="contact" className={`${this.props.article === 'contact' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
           <h2 className="major">Contact</h2>
-          <form action="/success" method="post" name="contact" data-netlify="true" data-netlify-honeypot="bot-field">
+          <form action="/success" netlify method="post" name="contact" data-netlify="true" data-netlify-honeypot="bot-field">
             <input type="hidden" name="bot-field" />
             <div className="field half first">
               <label htmlFor="name">Name</label>
