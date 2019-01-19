2b20911fa9708f6e46a4cdc29ec39f1d23db1138
Rafael Pina <rafaa.lb@hotmail.com>
2019-01-18 22:31:35 -0400

    Added updated cv

--- src/components/Header.js
+++ src/components/Header.js
@@ -43,7 +43,7 @@ const Header = (props) => (
             <ul>
                 <li><a href="javascript:;" onClick={() => {props.onOpenArticle('intro')}}>Intro</a></li>
                 <li><a href="javascript:;" onClick={() => {props.onOpenArticle('work')}}>Work</a></li>
-                <li><a href="javascript:;" onClick={() => {}}>Resumé</a></li>
+                <li><a href="javascript:;" onClick={() => window.location.href = '/cv.pdf'}>Resumé</a></li>
                 {false && <li><a href="javascript:;" onClick={() => {props.onOpenArticle('about')}}>Resumé</a></li>}
                 <li><a href="javascript:;" onClick={() => {props.onOpenArticle('contact')}}>Contact</a></li>
             </ul>
